df62452097ecc1da9acbc5aae107130d8865db40
SADmammoth <lo.ma200018@gmail.com>
2019-02-28 13:12:20 +0300

    feat: add function counting amount of zeros in "number!" record in "base" system

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  let num = number;
+  let div;
+  let pow = 0;
+  let min = 0;
+  let count;
+  
+  while(base > 1){
+
+    div = max_div(base);
+    pow = get_pow_of_div(div, base);
+    count = count_in_fact(div, num);
+    base/= parseInt(div ** pow);
+  
+    if(count/pow < min || min == 0){
+      min = parseInt(count/pow);
+    }
+
+  }
+  return min;
+}
+
+/* returns count of occurence of 'n' in 'num!' factorization */
+function count_in_fact(n, num){
+  let count = 0;
+  while(num >= n){
+      count += parseInt(num/n);
+      num /= n;
+ }
+ return  count;
+}
+
+/* returns maximum divider of 'number' */
+function max_div(number){
+  if(number == 1){
+    return 1;
+  }
+  if(is_simple(number)){
+    return number;
+  }
+  let i;
+  for(let i = 2; i < number / 2 + 1; i++){
+      if(number % i == 0 && is_simple(i)){
+          max = i;
+      }
+  }
+  return max;
+}
+
+/* checks if 'number' is simple */
+function is_simple(number){
+  for(let i = 2; i < number / 2 + 1; i++){
+    if(number % i == 0){
+      return false;
+    }
+}
+return true;
+}
+
+/* returns power of divider 'd' in factorization of 'number' */
+function get_pow_of_div(d, number){
+  let p;
+  number = number/d;
+  for(p = 1; number%d == 0; number/=d, p++);
+  if(p == 1){
+    return p;
+  }
+  return p;
 }
